69721f6c424807acd703fbcc5506b7e5928cfb5a
DFurtune <d.furtune18@gmail.com>
2024-11-07 12:23:56 +0200

    add Pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Pagination = ({
+  totalItems,
+  itemsPerPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  return (
+    <div className="pagination">
+      <button
+        disabled={currentPage === 1}
+        onClick={() => onPageChange(currentPage - 1)}
+      >
+        Previous
+      </button>
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button
+          key={index + 1}
+          className={currentPage === index + 1 ? "active" : ""}
+          onClick={() => onPageChange(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
+      <button
+        disabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
