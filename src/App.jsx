1e1cbdc6d4c191cbb32ad89136e9ac9ff7ee59a0
DFurtune <d.furtune18@gmail.com>
2024-11-07 12:28:06 +0200

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import TodoList from "./components/TodoList";
+import Pagination from "./components/Pagination";
+import { fetchTodos } from "./services/todoService";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const todosPerPage = 10;
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      const todosData = await fetchTodos();
+      setTodos(todosData);
+    };
+    loadTodos();
+  }, []);
+
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      <h1>TODO List</h1>
+      <TodoList todos={currentTodos} setTodos={setTodos} />
+      <Pagination
+        totalItems={todos.length}
+        itemsPerPage={todosPerPage}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
 
 export default App;
